fc60ad5af1b8e21a973d83e68616af3427f3eda4
hefeylcnky <78243283+CreateAce@users.noreply.github.com>
2024-10-21 01:22:10 +0300

    Add files via upload

---
+++ client.lua
@@ -0,0 +1,26 @@
+local cameraLockEnabled = false
+local fixedCameraPitch = 0.0  -- Sabit dikey açı
+
+RegisterCommand('kamerakilit', function()
+    cameraLockEnabled = not cameraLockEnabled
+    if cameraLockEnabled then
+        QBCore.Functions.Notify("Kamera kilidi açıldı", "success")
+        fixedCameraPitch = GetGameplayCamRelativePitch()
+    else
+        QBCore.Functions.Notify("Kamera kilidi kapandı", "error")
+    end
+end, false)
+
+CreateThread(function()
+    while true do
+        local sleep = 1000
+
+        if cameraLockEnabled then
+            sleep = 0 
+
+            SetGameplayCamRelativePitch(fixedCameraPitch, 0.0)
+        end
+
+        Wait(sleep)
+    end
+end)
---
+++ fxmanifest.lua
@@ -0,0 +1,11 @@
+fx_version 'cerulean'
+game 'gta5'
+lua54 'yes'
+
+
+client_scripts {
+	'client.lua'
+}
+
+--- XFiveM [Efe-Heray] tarafından ücretsiz paylaşılmış kamera kilit sistemidir.
+--- https://discord.gg/ChUB8Zm43n 
